a2044664eee1217f9f7b0406b1b817e84cc3b414
grisselfaura <grissel.faura@gmail.com>
2020-01-19 22:07:10 +0000

    sendemail custom link to sign in

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,21 @@
 /*Script send email code from code course adapted for this*/
 
-function sendMail(contactForm) {
-    emailjs.send("gmail", "grissel_resumen_test", {
-        "from_name": contactForm.fullname.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
+function sendMail(signInForm) {
+    emailjs.send("gmail", "template_vuxmZcCD", {
+        "from_name": signInForm.exampleInputName.value,
+        "from_email": signInForm.exampleInputEmail.value,
+        "from_last_name": signInForm.exampleInputLastName.value,
+        "from_country": signInForm.exampleInputCountry.value,
+        })
     .then(
         function(response) {
             console.log("SUCCESS", response);
         },
         function(error) {
             console.log("FAILED", error);
-        });
-}
+        }
+    );
+    return false;
+}    
+
+/*Script needs to refresh after send to be updated*/
