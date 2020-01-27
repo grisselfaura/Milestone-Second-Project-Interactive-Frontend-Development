89186c14cc68a187676bfa5742e083f711f4d156
grisselfaura <grissel.faura@gmail.com>
2020-01-27 22:20:17 +0000

    remove donut graph

--- assets/js/pie-graph.js
+++ assets/js/pie-graph.js
@@ -14,11 +14,6 @@ var arc = d3.arc()
     .outerRadius(radius - 10)
     .innerRadius(0);
 
-// donut chart arc
-var arc2 = d3.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 70);
-
 // arc for the labels position
 var labelArc = d3.arc()
     .outerRadius(radius - 40)
@@ -33,14 +28,7 @@ var pie = d3.pie()
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-// define the svg donut chart
-var svg2 = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // import data 
@@ -49,21 +37,22 @@ d3.csv("test-data.csv", function(error, data) {
     
     // parse data
     data.forEach(function(d) {
-        d.count = +d.count;
+        d.count = +d.count; // + convert value to an integer
         d.fruit = d.fruit;
-    })
+    });
 
   // "g element is a container used to group other SVG elements"
   var g = svg.selectAll(".arc")
       .data(pie(data))
-    .enter().append("g")
+      .enter().append("g")
       .attr("class", "arc");
 
   // append path 
   g.append("path")
       .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.fruit); })
-    // transition 
+      .style("fill", function(d) { return color(d.data.fruit); }) // or .style("fill","blue") 
+    
+      // transition 
     .transition()
       .ease(d3.easeLinear)
       .duration(2000)
@@ -78,42 +67,9 @@ d3.csv("test-data.csv", function(error, data) {
       .attr("dy", ".35em")
       .text(function(d) { return d.data.fruit; });
     
-
-    // "g element is a container used to group other SVG elements"
-  var g2 = svg2.selectAll(".arc2")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc2");
-
-   // append path 
-  g2.append("path")
-      .attr("d", arc2)
-      .style("fill", function(d) { return color(d.data.fruit); })
-    .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-      .attrTween("d", tweenDonut);
-        
-   // append text
-  g2.append("text")
-    .transition()
-      .ease(d3.easeLinear)
-      .duration(2000)
-    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.data.fruit; });
-    
-});
-
 // Helper function for animation of pie chart and donut chart
 function tweenPie(b) {
   b.innerRadius = 0;
   var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
   return function(t) { return arc(i(t)); };
-}
-
-function tweenDonut(b) {
-  b.innerRadius = 0;
-  var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
-  return function(t) { return arc2(i(t)); };
 }
