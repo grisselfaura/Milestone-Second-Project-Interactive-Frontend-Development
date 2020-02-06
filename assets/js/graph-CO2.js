cf6612ded96d32e744d8dfebf39c76cc2f96a136
grisselfaura <grissel.faura@gmail.com>
2020-02-06 00:20:56 +0000

    scale line chart

--- assets/js/graph-CO2.js
+++ assets/js/graph-CO2.js
@@ -10,25 +10,34 @@ function makeGraphs(error, emissionSectorData){ /*replace*/
     var ndx = crossfilter(emissionSectorData); /*replace*/ 
 
     emissionSectorData.forEach(function(d){
+        //console.log(d.Entity);
         d.year = parseInt(d.Year);
-        console.log(d.Year);
+        //console.log(d.Year);
         d.Transport = parseInt(d.Transport);
         d.Forestry = parseInt(d.Forestry);
+        d.Energy = parseInt(d.Energy);
         d.Other_sources = parseInt(d["Other sources"]);
-        console.log(d["Other sources"]);
-        d.yrs_since_phd = parseInt(d["yrs.since.phd"]); 
+        //console.log(d["Other sources"]);
+        d.Agriculture_Land_Use_Forestry = parseInt(d["Agriculture, Land Use and Forestry"]); 
+        //console.log(d["Agriculture, Land Use and Forestry"]);
+        d.Waste = parseInt(d.Waste);
+        d.Residential_commercial = parseInt(d["Residential and commercial"]);
+        //console.log(d["Residential and commercial"]);
         d.Industry = parseInt(d.Industry);
         d.Agriculture = parseInt(d.Agriculture);
     });
     
-
+    show_global_emissions_per_year(ndx);
     show_country_selector(ndx); //function takes the ndx crossfilter as its only argument
-   
+    show_country_emissions_top_sectors(ndx);
+    
+    dc.renderAll();
+}
+
+    
 function show_country_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('Entity'));
     var group = dim.group();
-
- 
     
     dc.selectMenu("#country-selector")
         .dimension(dim)
@@ -36,29 +45,44 @@ function show_country_selector(ndx) {
         .title(kv => kv.key);/*not showing the count numner*/
 }
 
+function show_global_emissions_per_year(ndx) {
+    var year_dim = ndx.dimension(dc.pluck('Year'));
+    var yearGlobalEmissionsChart = year_dim.group().reduceSum(function(d) {return d.cnt;});   /*REPLACE FOR sum co2 for all sectors*/ 
+     
+
+    /*for chart scale*/
+    var minYear = year_dim.bottom(1)[0].Year;
+    var maxYear = year_dim.top(1)[0].Year;
+    console.log(minYear);
+    console.log(maxYear);
+
+    dc.lineChart("#chart-global-CO2-year")
+            .width(1000)
+            .height(300)
+            .margins({top: 10, right: 50, bottom: 30, left:50})
+            .dimension(year_dim)
+            .group(yearGlobalEmissionsChart)
+            .transitionDuration(500)
+            .x(d3.time.domain([minYear, maxYear]))//check scale and x axus
+            .xAxisLabel("Years")
+            .yAxisLabel("Total CO2 emissions")
+            .yAxis().ticks(10);
+            // .title(function(d) {  // REMAIN UNCHANGED
+            //     return d.key[2] + " earned " + d.key[1]; 
+            // }) 
+            // .colorAccessor(function(d) {  // REMAIN UNCHANGED
+            //     return d.key[3]; 
+            // })    
+}
+
+function show_country_emissions_top_sectors(ndx) {
     var Entity_dim = ndx.dimension(dc.pluck('Entity'));/*replace by */ 
     var total_emissions_per_sector = Entity_dim.group().reduceSum(dc.pluck('Transport'));/*replace by country and sector*/ 
 
-    dc.pieChart("#emissions-per-sector") /*replace by name of div*/ 
+    dc.pieChart("#emissions-per-sector") /*replace by name of div this needs TO BE CHANGED TO SHOW TOP5 INDUSTIRES PER COUNTRY*/ 
         .height(330)
         .radius(90)
         .transitionDuration(1500)
         .dimension(Entity_dim) /*replace */ 
         .group(total_emissions_per_sector); /*replace by:  total_emissions_per_sector */ 
-    dc.renderAll();
 }
-
-// emissionSectorData.forEach(function(d){
-//         d.year = parseInt(d.year);
-//         d.Transport = parseInt(d.Transport);
-//         d.Forestry = parseInt(d.Forestry(tonnes));
-//         d.Energy  = parseInt(d.Energy(tonnes));
-//         d.Other = parseInt(d.Othersources(tonnes));
-//         d.Agriculture,_Land_Use_&_Forestry = parseInt(d.["Agriculture, Land Use & Forestry(tonnes)"]);
-//         d.Residential_&_commercial = parseInt(d.Residential & commercial(tonnes));
-//         d.Industry = parseInt(d.Industry(tonnes));
-//         d.Agriculture = parseInt(d.Agriculture(tonnes));
-        
-//         d.yrs_since_phd = parseInt(d["yrs.since.phd"])  //also to parse my yrs_since_phd from string to int.REMOVE DOTS TO AVOID PROBLEMS
-        
-//     });
