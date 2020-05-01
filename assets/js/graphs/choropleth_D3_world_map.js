936475d42cf4c49bc5368e712e312a8817ec12d6
grisselfaura <grissel.faura@gmail.com>
2020-05-01 17:48:25 +0000

    worldmap legend center

--- assets/js/graphs/choropleth_D3_world_map.js
+++ assets/js/graphs/choropleth_D3_world_map.js
@@ -10,36 +10,40 @@
         height = +svg.attr("height");
 
     // Define the div for the tooltip
-    var tooltip = body.append("div")
+    // var tooltip = d3.select("div.tooltip")
+    var tooltip = body.append("div") // var tooltip = svg.append("tooltip")?
         .attr("class", "tooltip")
         .style("opacity", 0);
 
     // Map and projection
     //Converting from geographic coordinates (longitude,latitude) to pixel coordinates is accomplished via a map projection. 
-    var path = d3.geoPath();
+    // var path = d3.geoPath();
     var projection = d3.geoNaturalEarth()
-        .scale(width / 2 / Math.PI)
-        .translate([width / 2, height / 2])
-    var path = d3.geoPath()
-        .projection(projection);
+        .scale(width / 2 / Math.PI) // scale things down so see world map
+        .translate([width / 2, height / 2]) // translate to center of screen
+    var path = d3.geoPath() //path generator that will convert GeoJSON to SVG paths
+        .projection(projection); // tell path generator to use NaturalEarth projection
         
     // Data and color scale
     var data = d3.map(); // CO emission Dataset
-    //console.log(data)
+        //console.log(data)
     var colorScheme = d3.schemePuOr[11]; // modified from tutorial
-    var colorScale = d3.scaleThreshold()
+    var colorScale = d3.scaleThreshold()// modified from tutorial
         .domain([-1000000, -100000, -10000, 10000, 100000, 1000000, 10000000, 100000000, 200000000, 500000000, 1000000000]) //  modify from original
         .range(colorScheme);
-        // Legend
+    
+    // Legend
     var g = svg.append("g")
         .attr("class", "legendThreshold")
-        .attr("transform", "translate(20,20)");
+        // .attr("transform", "translate(20,20)");
+        .attr("transform", "translate(50,80)"); //will move it to a different position relative to the other objects
     g.append("text")
         .attr("class", "caption")
         .attr("x", 0)
-        .attr("y", -6)
+        .attr("y", -10)
         .text("CO-2 levels");
-    var labels = ['-1000 000', '-100 000','-10 000', '10 000', '100 000', '1000 000', '10 000 000', '100 000 000', '200 000 000', '500 000 000', '> 1000 000 000']; //
+    // var labels = ['> 1000 000 000', '500 000 000', '200 000 000', '100 000 000', '10 000 000', '1000 000', '100 000', '10 000','-10 000', '-100 000','-1000 000']; // plus to minues
+    var labels = ['-1000 000', '-100 000','-10 000', '10 000', '100 000', '1000 000', '10 000 000', '100 000 000', '200 000 000', '500 000 000', '> 1000 000 000']; // minus to plus
     var legend = d3.legendColor()
         .labels(function (d) { return labels[d.i]; })
         .shapePadding(4)
@@ -75,7 +79,7 @@
         let mousemove = function(d) {
             d3.selectAll(".Country")
                 tooltip.style("opacity", 1)
-                    .html( d.properties.name + "<br>"+  d.total_CO )              
+                    .html( d.properties.name + "<br>" + d.total_CO )       //numberFormat(d.total_CO)       
                     .style("left", (d3.mouse(this)[0]+70) + "px")
                     .style("top", (d3.mouse(this)[1]) + "px")
         }
