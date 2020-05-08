28d3f12b7f3f96c1387e2faa2b5ec44c6e28ddbc
grisselfaura <grissel.faura@gmail.com>
2020-05-08 17:59:43 +0000

    clean choropleth world map code

--- assets/js/graphs/choropleth_D3_world_map.js
+++ assets/js/graphs/choropleth_D3_world_map.js
@@ -1,6 +1,10 @@
-    // original code http://bl.ocks.org/palewire/d2906de347a160f38bc0b7ca57721328    
-    // https://www.d3-graph-gallery.com/graph/choropleth_hover_effect.html
+/*Script using D3 to generate a world map choropleth. Code aid was taken from these tutorials:
+    http://bl.ocks.org/palewire/d2906de347a160f38bc0b7ca57721328    
+    https://www.d3-graph-gallery.com/graph/choropleth_hover_effect.html
+    https://jkeohan.wordpress.com/2015/03/09/using-d3-tooltips/ to indicate tooltip location
     
+*/
+   
      // Define body
     var body = d3.select("body");
 
@@ -8,8 +12,7 @@
     var svg = d3.select("svg"),
         width = +svg.attr("width"), 
         height = +svg.attr("height");
-        
-
+    
     // responsive https://help.visokio.com/support/solutions/articles/42000012405-how-to-create-a-custom-view-choropleth-map-example-using-d3
     //     var svg = d3.select("svg");
     //     var width = +parseInt(svg.style("width"), 10);
@@ -18,16 +21,12 @@
     //     var scale = maxSize/ORIGINAL_MAP_SIZE;  // ORIGINAL_MAP_SIZE is the maximum size (width or height) of the original map.
 
     // Define the div for the tooltip
-    // var tooltip = d3.select("div.tooltip")
     var tooltip = d3.select("body").append("div")   
-    // var tooltip = body.append("div") // var tooltip = svg.append("tooltip")?
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    // Map and projection
-    //Converting from geographic coordinates (longitude,latitude) to pixel coordinates is accomplished via a map projection. 
-    // var path = d3.geoPath();
-    var projection = d3.geoNaturalEarth()
+    // Map and projection 
+    var projection = d3.geoNaturalEarth() //Converting from geographic coordinates (longitude,latitude) to pixel coordinates is accomplished via a map projection. 
         .scale(width / 2 / Math.PI) // scale things down so see world map
         .translate([width / 2, height / 2]) // translate to center of screen
     var path = d3.geoPath() //path generator that will convert GeoJSON to SVG paths
@@ -36,22 +35,20 @@
     // Data and color scale
     var data = d3.map(); // CO emission Dataset
         //console.log(data)
-    var colorScheme = d3.schemePuOr[11]; // modified from tutorial
-    var colorScale = d3.scaleThreshold()// modified from tutorial
-        .domain([-1000000, -100000, -10000, 10000, 100000, 1000000, 10000000, 100000000, 200000000, 500000000, 1000000000]) //  modify from original
+    var colorScheme = d3.schemePuOr[11]; 
+    var colorScale = d3.scaleThreshold() 
+        .domain([-1000000, -100000, -10000, 10000, 100000, 1000000, 10000000, 100000000, 200000000, 500000000, 1000000000]) 
         .range(colorScheme);
     
     // Legend
     var g = svg.append("g")
         .attr("class", "legendThreshold")
-        // .attr("transform", "translate(20,20)");
-        .attr("transform", "translate(50,80)"); //will move it to a different position relative to the other objects
+        .attr("transform", "translate(50,80)"); // will move it to a different position relative to the other objects
     g.append("text")
         .attr("class", "caption")
         .attr("x", 0)
         .attr("y", -10)
         .text("CO-2 levels");
-    // var labels = ['> 1000 000 000', '500 000 000', '200 000 000', '100 000 000', '10 000 000', '1000 000', '100 000', '10 000','-10 000', '-100 000','-1000 000']; // plus to minues
     var labels = ['-1000 000', '-100 000','-10 000', '10 000', '100 000', '1000 000', '10 000 000', '100 000 000', '200 000 000', '500 000 000', '> 1000 000 000']; // minus to plus
     var legend = d3.legendColor()
         .labels(function (d) { return labels[d.i]; })
@@ -60,10 +57,9 @@
     svg.select(".legendThreshold")
         .call(legend);
 
-    // Load external data and boot, asynchronous tasks
+    // Load external (country coordinates + data) and boot, asynchronous tasks
     d3.queue()
         .defer(d3.json, "data/world_countries.json")
-        // .defer(d3.csv, "data/global-carbon-dioxide-emissions-by-sector_CLEAN.csv", function(d) {data.set( d.Code, +d.total_CO);})
         .defer(d3.csv, "data/global-carbon-dioxide-emissions-by-sector_CLEAN.csv", function(d) {if(d.Year == 2010) 
         {return (data.set(d.Code, +d.total_CO));}}) // process
         .await(ready);
@@ -73,7 +69,7 @@
         // console.log(topo);
         // console.log(CO_data);
 
-        let mouseOver = function(d) {
+        let mouseOver = function(d) { // retrieve data
             d3.selectAll(".Country")
                 .transition()
                 .duration(200)
@@ -88,19 +84,10 @@
         let mousemove = function(d) {
             d3.selectAll(".Country")
                 tooltip.style("opacity", 1)
-                    .html( d.properties.name + "<br>" + d.total_CO )       //numberFormat(d.total_CO)       
-                    // .style("left", (d3.mouse(this)[0]+70) + "px")
-                    // .style("top", (d3.mouse(this)[1]) + "px")
-                    .style("left", (d3.event.pageX) + "px")     //tooltip location https://jkeohan.wordpress.com/2015/03/09/using-d3-tooltips/
+                    .html( d.properties.name + "<br>" + d.total_CO )           
+                    .style("left", (d3.event.pageX) + "px") 
                     .style("top", (d3.event.pageY - 10) + "px");    
-        }
-
-        // .on("mouseover", function(d) {      
-        // tooltip.transition().duration(200).style("opacity", .9);      
-        // tooltip.html(d)  
-        // .style("left", (d3.event.pageX) + "px")     
-        // .style("top", (d3.event.pageY - 10) + "px");    
-        // })           
+        }         
 
         let mouseLeave = function(d) {
             d3.selectAll(".Country")
