e2c14b57103cb80752efd019ee473a409862d9d5
grisselfaura <grissel.faura@gmail.com>
2020-05-13 00:45:21 +0000

    bug fix except for percentage

--- assets/js/graphs/dashboard.js
+++ assets/js/graphs/dashboard.js
@@ -20,7 +20,8 @@ function makeGraphs(response) {  // Create a function called makeGraphs where th
     emissionData.forEach(function (d) {
         d.Code = String(d.Code);
         d.Entity = String(d.Entity);
-        d.Year = parseDate(d.Year).getFullYear(); // Using Full year for better date format display  for reduce functions"d.Year = parseDate(d.Year);"
+        d.Year = parseDate(d.Year);
+        // d.Year = parseDate(d.Year).getFullYear(); // Using Full year for better date format display  for reduce functions"d.Year = parseDate(d.Year);"
         d.Transport = Number(d.Transport); // Using Number() to get to get rid of empty values. Also possible parseFloat(d.Transport)|| 0; or d.Transport = d.Transport ? parseFloat(d.Transport) : 0
         d.Forestry = Number(d.Forestry);
         d.Energy = Number(d.Energy);
@@ -35,14 +36,14 @@ function makeGraphs(response) {  // Create a function called makeGraphs where th
     var ndx = crossfilter(emissionData); // Assign the data to a crossfilter to allow interaction
     var all = ndx.groupAll(); // Define group all for counting
 
-    showCOPercentagePerSector2010(ndx, "Transport", "#percent-CO-transport"); // Function takes the (ndx) crossfilter as its only argument
-    showCOPercentagePerSector2010(ndx, "Forestry", "#percent-CO-forestry");
-    showCOPercentagePerSector2010(ndx, "Energy", "#percent-CO-energy");
-    showCOPercentagePerSector2010(ndx, "Other_sources", "#percent-CO-other_sources");
-    showCOPercentagePerSector2010(ndx, "Agriculture_Land_Use_Forestry", "#percent-CO-agriculture_land_use_forestry");
-    showCOPercentagePerSector2010(ndx, "Waste", "#percent-CO-waste");
-    showCOPercentagePerSector2010(ndx, "Residential_commercial", "#percent-CO-residential_commercial");
-    showCOPercentagePerSector2010(ndx, "Industry", "#percent-CO-industry");
+    // showCOPercentagePerSector2010(ndx, "Transport", "#percent-CO-transport"); // Function takes the (ndx) crossfilter as its only argument
+    // showCOPercentagePerSector2010(ndx, "Forestry", "#percent-CO-forestry");
+    // showCOPercentagePerSector2010(ndx, "Energy", "#percent-CO-energy");
+    // showCOPercentagePerSector2010(ndx, "Other_sources", "#percent-CO-other_sources");
+    // showCOPercentagePerSector2010(ndx, "Agriculture_Land_Use_Forestry", "#percent-CO-agriculture_land_use_forestry");
+    // showCOPercentagePerSector2010(ndx, "Waste", "#percent-CO-waste");
+    // showCOPercentagePerSector2010(ndx, "Residential_commercial", "#percent-CO-residential_commercial");
+    // showCOPercentagePerSector2010(ndx, "Industry", "#percent-CO-industry");
     showCOAveragePerCountry(ndx);
     showWorldMap(ndx, topoData);
 
@@ -64,48 +65,49 @@ function makeGraphs(response) {  // Create a function called makeGraphs where th
  * @param {*} attr Sector
  * @param {*} element Percentage value number
  */
-function showCOPercentagePerSector2010(ndx, attr, element) {
-    // Define a dimension
-    var year_dim = ndx.dimension(d => d.Year);
+// function showCOPercentagePerSector2010(ndx, attr, element) {
+    var numberFormat = d3.format(".2f");  // Create a new format for the date
+//     // Define a dimension
+//     var year_dim = ndx.dimension(d => d.Year);
 
-    // Create functions to compute a value for any attribute.  
-    function reduceAddAvg(attr) {
-        return function (p, v) {
-            if (v.Year === 2010) { // Filter by specific year
-                p.sum_attr += v[attr];
-                p.sum_total_CO += v.total_CO;
-                p.percentage_attr = (p.sum_attr / p.sum_total_CO);
-            }
-            return p;
-        };
-    }
-    function reduceRemoveAvg(attr) {
-        return function (p, v) {
-            if (v.Year === 2010) {
-                p.sum_attr -= v[attr];
-                if (p.sum_attr == 0) {
-                    p.sum_total_CO -= v.total_CO;
-                    p.percentage_attr = 0;
-                } else {
-                    p.sum_total_CO -= v.total_CO;
-                    p.percentage_attr = (p.sum_attr / p.sum_total_CO);
-                }
-            }
-            return p;
-        };
-    }
-    function reduceInitAvg() {
-        return { sum_attr: 0, sum_total_CO: 0, percentage_attr: 0 };
-    }
+//     // Create functions to compute a value for any attribute.  
+//     function reduceAddAvg(attr) {
+//         return function (p, v) {
+//             if (v.Year === 2010) { // Filter by specific year
+//                 p.sum_attr += v[attr];
+//                 p.sum_total_CO += v.total_CO;
+//                 p.percentage_attr = (p.sum_attr / p.sum_total_CO);
+//             }
+//             return p;
+//         };
+//     }
+//     function reduceRemoveAvg(attr) {
+//         return function (p, v) {
+//             if (v.Year === 2010) {
+//                 p.sum_attr -= v[attr];
+//                 if (p.sum_attr == 0) {
+//                     p.sum_total_CO -= v.total_CO;
+//                     p.percentage_attr = 0;
+//                 } else {
+//                     p.sum_total_CO -= v.total_CO;
+//                     p.percentage_attr = (p.sum_attr / p.sum_total_CO);
+//                 }
+//             }
+//             return p;
+//         };
+//     }
+//     function reduceInitAvg() {
+//         return { sum_attr: 0, sum_total_CO: 0, percentage_attr: 0 };
+//     }
 
-    // Map/reduce to reduce function (percentages)  
-    var coPercentage = year_dim.group().reduce(reduceAddAvg(attr), reduceRemoveAvg(attr), reduceInitAvg);
-    dc.numberDisplay(element)
-        .useViewBoxResizing(true) // allows chart to be responsive (might need to add CSS 'width')
-        .formatNumber(d3.format(".2%"))
-        .valueAccessor((d) => d.value.percentage_attr)
-        .group(coPercentage)
-}
+//     // Map/reduce to reduce function (percentages)  
+//     var coPercentage = year_dim.group().reduce(reduceAddAvg(attr), reduceRemoveAvg(attr), reduceInitAvg);
+//     dc.numberDisplay(element)
+//         .useViewBoxResizing(true) // allows chart to be responsive (might need to add CSS 'width')
+//         .formatNumber(d3.format(".2%"))
+//         .valueAccessor((d) => d.value.percentage_attr)
+//         .group(coPercentage)
+// }
 
 /**
  * Function used to compute for each country (entity) the average value for all the sectors =  total CO value (during 1990-2010)
@@ -250,13 +252,10 @@ function showGlobalEmissionsPerYear(ndx) {
     
     // Map/reduce to group sum
     var yearGlobalEmissionsChart = year_dim.group().reduceSum(dc.pluck('total_CO'));
-        console.log(yearGlobalEmissionsChart.all());
-    
+        
         // For chart scale
     var minYear = year_dim.bottom(1)[0].Year;
     var maxYear = year_dim.top(1)[0].Year;
-    console.log(minYear); 
-    console.log(maxYear);
 
     dc.lineChart("#chart-global-CO2-year")
         // .width(500) // use carefully with .useViewBoxResizing
@@ -269,7 +268,6 @@ function showGlobalEmissionsPerYear(ndx) {
         .elasticY(true) // Allows scale to update with each other
         .yAxisPadding(100)
         .x(d3.scaleTime().domain([minYear, maxYear]))
-        // .tickFormat(d => d.getFullYear()))
         .xAxisLabel("Years")
         .yAxisLabel("Total CO2 emissions")
         .renderHorizontalGridLines(true)
